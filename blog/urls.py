2f11e3f5a3b4aef3f973b47f924cd598df412cc6
َAmirRamezani <amir20000.h.r@gmail.com>
2025-10-17 00:49:29 +0330

    categoriesblog_filter

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,9 @@ app_name ='blog'
 
 urlpatterns = [
     
-    # path ('url addree', 'view' , nam e)
+    # path ('url addree', 'view' , name)
    path('', blog_view, name='index'),
-   path('<int:pid>' ,blog_single ,name='single')
-    
+   path('<int:pid>' ,blog_single ,name='single'),
+   path('category/<str:cat_name>' ,blog_view ,name='category')
+   
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 # Create your views here.
-def blog_view(request):
-    # post = get_object_or_404(Post.objects.filter(status=1))
-    # context = {'posts':post}
-    
+def blog_view(request,cat_name=None):
     posts = Post.objects.filter(status=1)
-    return render(request, 'blog/blog-home.html', {'posts': posts})
-    # return render(request,'blog/blog-home.html',context)
+    if cat_name:
+        posts=posts.filter(categories__name=cat_name)
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html' ,context)
 
 def blog_single(request,pid):
-    # posts = Post.objects.filter(status=1)
-    # post = get_object_or_404(posts,pk=pid)
-    # post = get_object_or_404(Post.objects.select_related('category') ,pk=pid ,status=1)
     post = get_object_or_404(Post.objects.prefetch_related('categories'), pk=pid, status=1)
     context = {'post':post}
     return render(request,'blog/blog-single.html',context)
+
+
+def blog_category (request ,cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(categories__name=cat_name)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
