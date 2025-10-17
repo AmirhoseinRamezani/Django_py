5785efa90c9b1039d41b3c6e33116e3dfbc32018
َAmirRamezani <amir20000.h.r@gmail.com>
2025-10-17 05:19:42 +0330

    Pagination
    1-Pagination in View
    2-Show Pagination in Template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 from django.db.models import Q
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 def blog_view(request,**kwargs):
     # Using kwargs/args for code optimization and readability
@@ -9,6 +10,14 @@ def blog_view(request,**kwargs):
         posts=posts.filter(categories__name=kwargs['cat_name'])
     if kwargs.get('author_username') !=None:
         posts=posts.filter(author__username = kwargs['author_username'])
+    posts = Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html' ,context)
 
@@ -40,5 +49,4 @@ def blog_search(request):
             'query': query,
             # 'results_count': posts.count()  # نمایش تعداد نتایج
             }
-    print (context)
     return render(request, 'blog/blog-home.html', context)
